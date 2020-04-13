23e711474210c5f23157b61f1416d74e08607e6e
Michael Cassaro <mdcassaro@hotmail.com>
2020-04-13 14:47:57 -0500

    added prompts

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "project",
+        message: "What is your project's name?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project?"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What kind of license should your project have?"
+      },
+      {
+        type: "input",
+        name: "repo",
+        message: "What does the user need to know about using your repo?"
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"
+      }
+    ]);
+  }
+
 const questions = [
 
 ];
 
-function writeToFile(fileName, data) {
+function writeToFile("README.md", text) {
 }
 
 function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      const text = generateHTML(answers);
+  
+      await writeFileAsync("README.md", Markdown);
+  
+      console.log("Successfully wrote to README.md");
+    } catch(err) {
+      console.log(err);
+    }
 
 }
 
